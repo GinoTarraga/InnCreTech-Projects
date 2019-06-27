c2a480c2b81677245347d23323a4d5b657c8eccb
Gino Tarraga <ginotarraga@gmail.com>
2019-06-27 14:09:35 -0400

    Update TaskService.java

--- demo 5test/src/main/java/com/example/demo/service/TaskService.java
+++ demo 5test/src/main/java/com/example/demo/service/TaskService.java
@@ -4,9 +4,13 @@ import com.example.demo.dtos.EmployeeDTO;
 import com.example.demo.dtos.TaskDTO;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public interface TaskService {
 
     TaskDTO saveTask(TaskDTO taskDTO);
 
     TaskDTO assignTask(TaskDTO taskDTO);
+
+    List <TaskDTO> completedTaskList (TaskDTO taskDTO);
 }
