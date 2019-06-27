766339cd0f42e990ff75cb571ab622b1b93c26cb
Gino Tarraga <ginotarraga@gmail.com>
2019-06-27 14:07:04 -0400

    Update Employee.java

--- demo 5test/src/main/java/com/example/demo/entity/Employee.java
+++ demo 5test/src/main/java/com/example/demo/entity/Employee.java
@@ -1,6 +1,7 @@
 package com.example.demo.entity;
 
 import com.example.demo.dtos.TaskDTO;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
@@ -14,11 +15,21 @@ import java.util.List;
 public class Employee {
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Column(name = "employee_Id")
 	private long employeeId;
 
+	@Column(name = "employee_Name")
 	private String employeeName;
 
+	@Column(name = "task_id")
+	private long taskId;
+
+	@Column(name = "task_Name")
 	private String taskName;
+
+	@Column(name = "mark_completed")
+	@JsonProperty
+	private boolean isCompleted; //
 
 	public long getEmployeeId() {
 		return employeeId;
@@ -32,8 +43,17 @@ public class Employee {
 		this.employeeName = employeeName;
 	}
 
+	//Task getters & Setters
 	public String getTaskName() { return taskName; }
 
 	public void  setTaskName(String taskName) {this.taskName = taskName; }
 
+	public long getTaskId() {return taskId; }
+
+	public void setTaskId(long taskId) {this.taskId = taskId;}
+
+	//isCompleted getters & Setters
+	public boolean getIsCompleted() { return isCompleted; }
+
+	public void setIsCompleted(boolean isCompleted){ this.isCompleted = isCompleted; }
 }
