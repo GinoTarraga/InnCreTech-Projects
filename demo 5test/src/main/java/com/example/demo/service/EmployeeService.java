e3ac35173a43253523161f2bcf418cc1f2959f59
Gino Tarraga <ginotarraga@gmail.com>
2019-06-27 14:09:16 -0400

    Update EmployeeService.java

--- demo 5test/src/main/java/com/example/demo/service/EmployeeService.java
+++ demo 5test/src/main/java/com/example/demo/service/EmployeeService.java
@@ -2,10 +2,14 @@ package com.example.demo.service;
 
 import com.example.demo.dtos.EmployeeDTO;
 import com.example.demo.dtos.TaskDTO;
+import com.example.demo.entity.Employee;
 import com.example.demo.entity.Task;
 
 public interface EmployeeService {
 
 	EmployeeDTO saveEmployee(EmployeeDTO employeeDTO);
+
+	EmployeeDTO isCompleted(EmployeeDTO employeeDTO);
+
 
 }
