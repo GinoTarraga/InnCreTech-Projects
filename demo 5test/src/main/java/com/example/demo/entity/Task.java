e30207c776e125df44a7b09c6eaaccf1a7a579b5
Gino Tarraga <ginotarraga@gmail.com>
2019-06-27 14:07:47 -0400

    Update Task.java

--- demo 5test/src/main/java/com/example/demo/entity/Task.java
+++ demo 5test/src/main/java/com/example/demo/entity/Task.java
@@ -1,6 +1,7 @@
 package com.example.demo.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
@@ -17,13 +18,22 @@ public class Task implements Serializable {
 private static final long serialVersionUID =1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Auto generates uses a global number generater for every entity
+    @Column(name = "task_Id")
     private long taskId;
 
+    @Column(name = "task_Name")
     private String taskName;
 
+    @Column(name = "employee_Name")
     private String employeeName;
 
+    @Column(name = "employee_Id")
     private long employeeId;
+
+
+    @Column(name = "marked_Completed")
+    @JsonProperty
+    private boolean isCompleted;
 
     public long getTaskId(){ return taskId; }
 
@@ -31,13 +41,19 @@ private static final long serialVersionUID =1L;
 
     public void setTaskName(String taskName){ this.taskName = taskName; }
 
-   public String getEmployeeName() { return employeeName; }
+    //Employee getters & Setters
+    public String getEmployeeName() { return employeeName; }
 
-   public void setEmployeeName(String employeeName){ this.employeeName = employeeName; }
+    public void setEmployeeName(String employeeName){ this.employeeName = employeeName; }
 
-   public long getEmployeeId(){ return employeeId; }
+    public long getEmployeeId(){ return employeeId; }
 
-   public void setEmployeeId(Long employeeId) {this.employeeId = employeeId; }
+    public void setEmployeeId(Long employeeId) {this.employeeId = employeeId; }
+
+    //isCompleted getters & Setters
+    public boolean getIsCompleted() { return isCompleted; }
+
+    public void setIsCompleted(boolean isCompleted){ this.isCompleted = isCompleted; }
 
 
 }
