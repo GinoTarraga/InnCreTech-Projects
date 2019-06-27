a908742abcdbcccced0cf9fbaa6798aab83dd5b4
Gino Tarraga <ginotarraga@gmail.com>
2019-06-27 14:10:25 -0400

    Update Tasklmpl.java

--- demo 5test/src/main/java/com/example/demo/serviceImpl/Tasklmpl.java
+++ demo 5test/src/main/java/com/example/demo/serviceImpl/Tasklmpl.java
@@ -10,6 +10,8 @@ import com.example.demo.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class Tasklmpl implements TaskService {
 
@@ -25,5 +27,11 @@ public class Tasklmpl implements TaskService {
     public TaskDTO assignTask(TaskDTO taskDTO){
         return taskManager.assignTask(taskDTO);
     }
+
+    @Override
+    public List<TaskDTO> completedTaskList(TaskDTO taskDTO) {
+        return taskManager.completedTaskList(taskDTO);
+    }
+
 
 }
